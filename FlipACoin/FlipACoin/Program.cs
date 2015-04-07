c2994307ae83eff1a109ae728873cf132a7e41ee
David Knoll <knolladavid@gmail.com>
2015-04-07 17:00:02 -0600

    flipforheads

--- FlipACoin/FlipACoin/Program.cs
+++ FlipACoin/FlipACoin/Program.cs
@@ -14,15 +14,26 @@ namespace FlipACoin
 
         static void Main(string[] args)
         {
-          
+
 
             for (int i = 0; i < 1000; i++)
-			{
-			   Console.WriteLine("Flipped: " + FlipACoin()); 
-			}
+            {
+                Console.WriteLine("Flipped: " + FlipACoin());
+            }
             Console.WriteLine("#Tails: " + tailsCounter);
             Console.WriteLine("#Heads: " + headsCounter);
+            Console.WriteLine("It took " + FlipForHeads() + " times to get a head");
             Console.ReadKey();
+        }
+            public static int FlipForHeads()
+            {
+               int counter =1;
+                //Flip while it is not a Head
+                while(rng.Next(2) != 0)
+                {
+                    counter++;
+                }
+                return counter;
         }
         public static string FlipACoin() 
         {
